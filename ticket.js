d1a17059d207087a494ad456d962ebf99068138d
rlazic <118766710+rlazic@users.noreply.github.com>
2023-05-11 16:02:58 +0200

    Update ticket.js

--- ticket.js
+++ ticket.js
@@ -1,1 +1,18 @@
+class Ticket {
+  constructor(title, description, assignee, dueDate, labels) {
+    this.title = title;
+    this.description = description;
+    this.assignee = assignee;
+    this.dueDate = dueDate;
+    this.labels = labels;
+  }
+}
+
+const editable = document.querySelector('.editable');
+
+editable.addEventListener('input', function() {
+  console.log('Text changed:', editable.innerText);
+});
+
+
 
