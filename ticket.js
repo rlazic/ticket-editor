49752800aa525f0c9a04136bab45530d4776c5d6
rlazic <118766710+rlazic@users.noreply.github.com>
2023-05-13 14:05:51 +0200

    Update ticket.js

--- ticket.js
+++ ticket.js
@@ -1,19 +1,22 @@
+/*Defining the ticket class with its components*/
 class Ticket {
-  constructor(title, description, assignee, dueDate, labels) {
-    this.title = title;
-    this.description = description;
-    this.assignee = assignee;
-    this.dueDate = dueDate;
-    this.labels = labels;
+ constructor(title, description, assignee, dueDate, labels) {
+  this.title = title;
+  this.description = description;
+  this.assignee = assignee;
+  this.dueDate = dueDate;
+  this.labels = labels;
   }
 }
 
+/*Editable ticket title*/
 const editable = document.querySelector('.editable');
 
 editable.addEventListener('input', function() {
   console.log('Text changed:', editable.innerText);
 });
 
+/*Due date selected from a dropdow calendar*/
 const dueDateInput = document.getElementById('due-date');
 
 dueDateInput.addEventListener('change', function(event) {
